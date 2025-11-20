ed28293c58d11090a7af359ca6a4550f3f95fcf7
Nikolas Valsamidis <nv222dv@student.lnu.se>
2025-11-20 13:45:12 +0100

    added a run sequence for plotting the profiles

--- project_run/BroadeningProfilesPlots.py
+++ project_run/BroadeningProfilesPlots.py
@@ -4,7 +4,7 @@ from project_classes.Molecule import Molecule
 from project_classes.BroadeningProfile import BroadeningProfile
 from astropy import units as u
 from matplotlib import pyplot as plt
-
+import numpy as np
 
 Na   = Molecule('Na', 5800*u.AA, 6000*u.AA)
 b    = 0.01 * u.km/u.s          # Doppler parameter
@@ -24,41 +24,24 @@ phi_L = prof_L.sigmaArray_sym[iline, :].value
 lam_V = prof_V.lam_sym[iline, :].to(u.AA).value
 phi_V = prof_V.sigmaArray_sym[iline, :].value
 
-lam0 = Na.lam0[iline].to(u.AA).value   # line-centre wavelength
+lam0 = Na.lam0[iline][0].to(u.AA).value   # line-centre wavelength
+lim = b.value/10
 
-plt.figure()
-plt.plot(lam_G, phi_G, label='Gaussian', color = 'grey')
-plt.plot(lam_L, phi_L, label='Lorentzian', color = 'lightgrey')
-plt.plot(lam_V, phi_V, label='Voigt', color = 'black')
+plt.figure(figsize=(8, 5)) 
+plt.plot(lam_G, phi_G, lw=0.9, alpha=1, label='Gaussian', color = 'goldenrod')
+plt.plot(lam_L, phi_L, lw=0.9, alpha=1, label='Lorentzian', color = 'teal')
+plt.plot(lam_V, phi_V, lw=0.9, alpha=1, label='Voigt', color = 'darkred')
 plt.xlabel(r'Wavelength $\lambda$ [Å]')
-plt.ylabel(r'Profile $\phi(\lambda)$')
-plt.title('Gaussian, Lorentzian and Voigt profiles')
-plt.xlim(lam0 - 0.001, lam0 + 0.001)       # only ±0.1 Å around line centre
+plt.ylabel(r'Normalized Profile $\phi(\lambda)$')
+plt.title(rf'Broadening profiles for Na, b = {b}')
+plt.xlim(lam0 - lim, lam0 + lim)       # only ±0.001 Å around line centre
+plt.xticks([lam0-lim, lam0, lam0+lim], [rf"-{lim}", rf"$\lambda_0= {lam0}$", rf"+{lim}"])
+plt.yticks([])
 plt.legend()
 plt.tight_layout()
+plt.savefig("Plots/BroadeningProfiles.pdf")
 plt.show()
 
-plt.figure()
-b_values = [0.01, 0.005, 0.001]   # km/s, broad → very narrow
 
-for b_val in b_values:
-    b_here = b_val * u.km/u.s
-    prof_V = BroadeningProfile(Na, b_here, vlim, Npts, 'voigt')
-
-    lam = prof_V.lam_sym[iline, :].to(u.AA).value
-    phi = prof_V.sigmaArray_sym[iline, :].value
-
-    plt.plot(lam, phi, label=fr'$b = {b_val:.3f}\,\mathrm{{km\,s^{{-1}}}}$')
-
-plt.plot(lam_G, phi_G, label='Gaussian', color='grey')
-plt.plot(lam_L, phi_L, label='Lorentzian', color='lightgrey')
-
-plt.xlabel(r'Wavelength $\lambda$ [Å]')
-plt.ylabel(r'Voigt profile $\phi_V(\lambda)$')
-plt.title('Voigt profiles for different Doppler parameter $b$')
-plt.xlim(lam0 - 0.001, lam0 + 0.001)       # zoom in
-plt.legend()
-plt.tight_layout()
-plt.show()
-
+plt.figure(figsize=(8, 5))
 
